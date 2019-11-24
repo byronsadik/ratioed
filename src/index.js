897736e6a0ff5822c27eccca895c810def3ae901
Byron <byron@attoconsulting.com>
2019-11-23 19:57:29 -0500

    added test callout for Twitter API

--- src/index.js
+++ src/index.js
@@ -10,3 +10,26 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+fetch("https://twitterbukativ1.p.rapidapi.com/search", {
+    "method": "POST",
+    "headers": {
+        "x-rapidapi-host": "TwitterBukatiV1.p.rapidapi.com",
+        "x-rapidapi-key": "4667ebdb75mshb897f762a157513p1f5b5djsn96c4d9bc87ed",
+        "content-type": "application/x-www-form-urlencoded"
+    },
+    "body": {
+        "consumerKey": "iKl6TjR15jVIaBNjRwFq5MHhH",
+        "query": "cheese",
+        "accessTokenKey": "1198396973499461632-5GkDiWJaKh8aWsfX7nn5potUWljspH",
+        "consumerSecret": "GyJcWqINto93ooCnrwWNmdiLPpFJVIEAfl6zsC13rzeg7nfJxc",
+        "accessTokenSecret": "X0GP39wh9fuyeP4mcOpnhR5GWCeyxw0s0Rh0ZS63JXMmc"
+    }
+})
+.then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.log(err);
+});
